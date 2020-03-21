2940efb28c60754d8618921d021877b18a3cde68
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-21 17:37:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,10 +29,10 @@ function removeElementFromBeginningOfArray (array) {
   return array;
 }
 
-// function removeElementFromEndOfArray (array) {
-//   array = array.slice(1,-1);
-//   return array;
-//}
+function removeElementFromEndOfArray (array) {
+  array = array.slice(1,-1);
+  return array;
+}
 
 function destructivelyRemoveElementFromBeginningOfArray (array) {
   array.shift();
