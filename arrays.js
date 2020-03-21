102c3b3c685864ccf564c5c1be154664e0919dae
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-21 17:36:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function removeElementFromBeginningOfArray (array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  array = array.slice(-1,-2);
+  array = array.slice(1,-1);
   return array;
 }
 
